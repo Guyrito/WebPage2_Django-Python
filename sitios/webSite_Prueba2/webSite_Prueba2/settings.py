3dcb44659c488c6d11c2c0483c54a7d4397bccd6
ritoDuocGuy <dieg.ortegav@alumnos.duoc.cl>
2020-12-09 00:44:40 -0300

    se agrego la propiedad de edición a las redes sociales - social

--- sitios/webSite_Prueba2/webSite_Prueba2/settings.py
+++ sitios/webSite_Prueba2/webSite_Prueba2/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'contact',
     'ckeditor',
+    'social.apps.SocialConfig',
     'games.apps.GamesConfig',
 ]
 
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.ctx_dict',
             ],
         },
     },
